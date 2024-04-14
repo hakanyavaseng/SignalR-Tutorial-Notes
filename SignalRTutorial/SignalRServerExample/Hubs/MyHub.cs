f0da6b456badcf5a43eae086ef67082769d0183c
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-04-14 22:31:18 +0300

    simple server and client in SignalR

---
+++ SignalRTutorial/SignalRServerExample/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR; // Comes with .NET Core as default
+
+namespace SignalRServerExample.Hubs
+{
+    public class MyHub : Hub //Name convention
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message); // Clients property comes with Hub base class, All sends the message to all connected clients.
+        }
+    }
+}
---
+++ SignalRTutorial/SignalRServerExample/Program.cs
@@ -0,0 +1,29 @@
+using SignalRServerExample.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddCors(options =>
+    {
+        options.AddDefaultPolicy(policy =>
+        {
+            policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials()
+            .SetIsOriginAllowed(origin => true);
+        });
+    });
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseCors();
+app.MapHub<MyHub>("/myhub"); 
+/*
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<MyHub>("/myhub");
+});
+*/
+app.Run();
