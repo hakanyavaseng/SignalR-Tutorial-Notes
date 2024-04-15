8c153d0e937a042444f850ec88a5d114c7bfa799
Hakan Yavaş <hakanyavaseng@gmail.com>
2024-04-15 21:04:50 +0300

    IHubContext interface implemented.

---
+++ SignalRTutorial/SignalRServerExample/Business/MyBusiness.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRServerExample.Hubs;
+
+namespace SignalRServerExample.Business
+{
+    public class MyBusiness
+    {
+        IHubContext<MyHub> hubContext;
+
+        public MyBusiness(IHubContext<MyHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        }
+        
+        // To do business logic IHubContext interface can be used.
+        public async Task SendMessageAsync(string message)
+        {
+            await hubContext.Clients.All.SendAsync("receiveMessage", message);
+        }
+        
+
+
+    }
+}
---
+++ SignalRTutorial/SignalRServerExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalRServerExample.Business;
+
+namespace SignalRServerExample.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly MyBusiness myBusiness;
+
+        public HomeController(MyBusiness myBusiness)
+        {
+            this.myBusiness = myBusiness;
+        }
+
+        [HttpGet("{message}")]
+        public async Task<IActionResult> Index([FromRoute] string message)
+        {
+            await myBusiness.SendMessageAsync("Hello!");
+            return Ok();
+        }
+    }
+}
